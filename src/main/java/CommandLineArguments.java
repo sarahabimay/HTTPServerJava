ed7f94836cfedb592cfef59081361ffbddf086ce
sarahabimay <sarahabimay@gmail.com>
2016-04-29 11:28:26 +0100

    Move Port and PublicDir command line argument processing.

---
+++ src/main/java/CommandLineArguments.java
@@ -0,0 +1,31 @@
+public class CommandLineArguments {
+    public static int currentPort = 5000;
+    public static String currentPublicClassPath = "/Users/sarahjohnston/Sarah/CobSpec/public/";
+
+    public void process_arguments(String[] args) {
+        for (int i = 0; i < args.length; i++) {
+            portNumber(i, args);
+            publicClassPath(i, args);
+        }
+    }
+
+    public int portNumber() {
+        return currentPort;
+    }
+
+    public String publicClassPath() {
+        return currentPublicClassPath;
+    }
+
+    private static void publicClassPath(int index, String[] args) {
+        if (args[index] == "-d") {
+            currentPublicClassPath = args[index + 1];
+        }
+    }
+
+    private static void portNumber(int index, String[] args) {
+        if (args[index] == "-p") {
+            currentPort = Integer.parseInt(args[index + 1]);
+        }
+    }
+}
--- src/main/java/HttpServerMain.java
+++ src/main/java/HttpServerMain.java
@@ -1,37 +1,20 @@
+import java.io.IOException;
+import java.net.ServerSocket;
+
 public class HttpServerMain {
-    public static int currentPort = 5000;
-    public static String currentPublicClassPath = "/Users/sarahjohnston/Sarah/CobSpec/public/";
-
     public static void main(String[] args) {
-        serverHostAndPort(args);
-        HttpServer server = new HttpServer(currentPort);
-        server.serverUp();
-    }
-
-    public int portNumber() {
-        return currentPort;
-    }
-
-    public String publicClassPath() {
-        return currentPublicClassPath;
-    }
-
-    private static void serverHostAndPort(String[] args) {
-        for (int i = 0; i < args.length; i++) {
-            portNumber(i, args);
-            publicClassPath(i, args);
+        CommandLineArguments arguments = commandLineArguments(args);
+        try {
+            HttpServer server = new HttpServer(new HttpServerSocket(new ServerSocket(arguments.portNumber())));
+            server.serverUp();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 
-    private static void publicClassPath(int index, String[] args) {
-        if (args[index] == "-d") {
-            currentPublicClassPath = args[index + 1];
-        }
-    }
-
-    private static void portNumber(int index, String[] args) {
-        if (args[index] == "-p") {
-            currentPort = Integer.parseInt(args[index + 1]);
-        }
+    private static CommandLineArguments commandLineArguments(String[] args) {
+        CommandLineArguments arguments = new CommandLineArguments();
+        arguments.process_arguments(args);
+        return arguments;
     }
 }
